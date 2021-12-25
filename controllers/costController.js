2f71b355d661b6b54ea863bfbe934aadc88b7c48
Corey <simon8888xsimon8888x@gmail.com>
2021-12-26 05:21:26 +0800

    add costQuery-month function

--- controllers/costController.js
+++ controllers/costController.js
@@ -84,100 +84,162 @@ const costController = {
 
   getCostQueryRange: (req, res) => {
     d('PAR queryItem', req.params)
-    Payment.findAll({
-      raw: true, nest: true, where: { isShare: false, userId: req.user.id },
-      include: [{ model: Category }]
-    })
-      .then((payments) => {
-        // d('week', moment(payments[0].createdAt).format('d:w'))
-        // 將所有資料依照週/類別進行排序
-        // payments.forEach((payment,i) => {
-        //   //找尋是否已創建過資料
-        //   let index = sortResult.findIndex((data) => {
-        //     return (data.category === payment.Category.name) &&
-        //       (data.week === moment(payment.createdAt).week())
-        //   })
-        //   // 如果沒有資料則將價格往上加
-        //   if (index !== -1) {
-        //     return sortResult[index] ? sortResult[index].price += payment.price : ""
-        //   } 
-        //   //創建新的資料區
-        //   let time = moment(payment.createdAt)
-        //   sortResult.push({
-        //     week: time.week(),
-        //     price: payment.price,
-        //     category: payment.Category.name,
-        //     startDate: time.add(1 - (time.format('d')), 'days').format(),
-        //     endDate: time.add(7 - (time.format('d')), 'days').format()
-        //   })
-        // })
-        // d('payment', payments[0])
-        const sortResult = []
-        payments.forEach((payment) => {
-          let status = 0 // 0:無日期資料 1:有資料無類別 2:有資料有類別
-          let index = -1
-          let categoryIndex = -1
+    const { queryItem } = req.params
+    switch (queryItem) {
 
-          //找尋是否已創建過資料
-          sortResult.forEach((data, i) => {
-            if (data.week === moment(payment.createdAt).week()) {
-              status = 1
-              index = i
-              if (data.category.find((k, i) => {
-                if (k.name === payment.Category.name) {
-                  categoryIndex = i
-                  return true
+      case 'week':
+        Payment.findAll({
+          raw: true, nest: true, where: { isShare: false, userId: req.user.id },
+          include: [{ model: Category }]
+        })
+          .then((payments) => {
+            const sortResult = []
+            payments.forEach((payment) => {
+              let status = 0 // 0:無日期資料 1:有資料無類別 2:有資料有類別
+              let index = -1
+              let categoryIndex = -1
+
+              //找尋是否已創建過資料
+              sortResult.forEach((data, i) => {
+                if (data.week === moment(payment.createdAt).week()) {
+                  status = 1
+                  index = i
+                  if (data.category.find((k, i) => {
+                    if (k.name === payment.Category.name) {
+                      categoryIndex = i
+                      return true
+                    }
+                  })) {
+                    status = 2
+                  }
                 }
-              })) {
-                status = 2
+              })
+
+              //根據上敘定義作不同處置
+              if (status === 0) {
+                let time = moment(payment.createdAt)
+                sortResult.push({
+                  week: time.week(),
+                  price: payment.price,
+                  category: [{
+                    name: payment.Category.name,
+                    price: payment.price,
+                    percentage: 0
+                  }],
+                  startDate: time.add(1 - (time.format('d')), 'days').format(),
+                  endDate: time.add(7 - (time.format('d')), 'days').format()
+                })
+              } else if (status === 1) {
+                sortResult[index].price += payment.price
+                sortResult[index].category.push({
+                  name: payment.Category.name,
+                  price: payment.price,
+                  percentage: 0
+                })
+              } else {
+                sortResult[index].price += payment.price
+                sortResult[index].category[categoryIndex].price += payment.price
               }
-            }
-          })
-
-          //根據上敘定義作不同處置
-          if (status === 0) {
-            let time = moment(payment.createdAt)
-            sortResult.push({
-              week: time.week(),
-              price: payment.price,
-              category: [{
-                name: payment.Category.name,
-                price: payment.price,
-                percentage: 0
-              }],
-              startDate: time.add(1 - (time.format('d')), 'days').format(),
-              endDate: time.add(7 - (time.format('d')), 'days').format()
             })
-          } else if (status === 1) {
-            sortResult[index].price += payment.price
-            sortResult[index].category.push({
-              name: payment.Category.name,
-              price: payment.price,
-              percentage: 0
+
+            // 計算佔比(之後整合到上面)
+            sortResult.forEach(d => {
+              d.category.forEach((unit) => {
+                unit.percentage = ((unit.price / d.price) * 100).toFixed(2)
+              })
+              // 計算前五
+              d.category.sort((x, y) => y.price - x.price)
             })
-          } else {
-            sortResult[index].price += payment.price
-            sortResult[index].category[categoryIndex].price += payment.price
-          }
-        })
 
-        // 計算佔比(之後整合到上面)
-        sortResult.forEach(d => {
-          d.category.forEach((unit) => {
-            unit.percentage = ((unit.price / d.price) * 100).toFixed(2)
+            // 只留類別佔比 top5
+            sortResult.forEach((d) => {
+              d.category.splice(5, d.category.length - 5)
+            })
+            // d('sortResult - final', sortResult)
+            res.render('costQueryWeek', { sortResult })
           })
-          // 計算前五
-          d.category.sort((x, y) => y.price - x.price)
-        })
+          .catch((err) => { console.log(err) })
+        break;
 
-        // 只留類別佔比 top5
-        sortResult.forEach((d) => {
-          d.category.splice(5, d.category.length - 5)
+      case 'month':   ////////////////////////////////////////////////////////////////
+        Payment.findAll({
+          raw: true, nest: true, where: { isShare: false, userId: req.user.id },
+          include: [{ model: Category }]
         })
-        // d('sortResult - final', sortResult)
-        res.render('costQueryWeek', { sortResult })
-      })
-      .catch((err) => { console.log(err) })
+          .then((payments) => {
+            const sortResult = []
+            payments.forEach((payment) => {
+              let status = 0 // 0:無日期資料 1:有資料無類別 2:有資料有類別
+              let index = -1
+              let categoryIndex = -1
+          
+              //找尋是否已創建過資料
+              sortResult.forEach((data, i) => {
+
+                if (data.month === moment(payment.createdAt).month()) {
+                  status = 1
+                  index = i
+                  if (data.category.find((k, i) => {
+                    if (k.name === payment.Category.name) {
+                      categoryIndex = i
+                      return true
+                    }
+                  })) {
+                    status = 2
+                  }
+                }
+              })
+
+              //根據上敘定義作不同處置
+              if (status === 0) {
+                let time = moment(payment.createdAt)
+                sortResult.push({
+                  month: time.month(),
+                  price: payment.price,
+                  category: [{
+                    name: payment.Category.name,
+                    price: payment.price,
+                    percentage: 0
+                  }],
+                  startDate: time.format('YYYY-MM')
+                })
+              } else if (status === 1) {
+                sortResult[index].price += payment.price
+                sortResult[index].category.push({
+                  name: payment.Category.name,
+                  price: payment.price,
+                  percentage: 0
+                })
+              } else {
+                sortResult[index].price += payment.price
+                sortResult[index].category[categoryIndex].price += payment.price
+              }
+            })
+
+            // 計算佔比(之後整合到上面)
+            sortResult.forEach(d => {
+              d.category.forEach((unit) => {
+                unit.percentage = ((unit.price / d.price) * 100).toFixed(2)
+              })
+              // 計算前五
+              d.category.sort((x, y) => y.price - x.price)
+            })
+
+            // 只留類別佔比 top5
+            sortResult.forEach((d) => {
+              d.category.splice(5, d.category.length - 5)
+            })
+            // d('sortResult - MONTH', sortResult)
+            // d('sortResult - MONTH', sortResult[0].category)
+            res.render('costQueryMonth', { sortResult })
+          })
+          .catch((err) => { console.log(err) })
+        break;
+
+      default:
+        break;
+    }
+   
   },
 
 
