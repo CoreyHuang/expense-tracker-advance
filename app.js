403131df42b28f625f8bb826a8a780fe1d369512
Corey <simon8888xsimon8888x@gmail.com>
2022-01-01 05:10:49 +0800

    add dotenv file and function

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const { engine } = require('express-handlebars');
 const session = require('express-session')
 const app = express()
-const post = 3000
 const passport = require('./config/passport')
 const flash = require('connect-flash')
 const d = require('./components/debug')
@@ -11,10 +10,16 @@ app.engine('handlebars', engine({ helpers: require('./config/handlebars-helper')
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+const port = process.env.PORT || 3000
+const sessionSecret = process.env.SECRET || 'corey'
+
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(session({ secret: 'corey', resave: false, saveUninitialized: true }));
+app.use(session({ secret: sessionSecret, resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash())
@@ -27,7 +32,7 @@ app.use((req, res, next) => {
   next()
 })
 
-app.listen(post, () => {
+app.listen(port, () => {
   console.log('server is enable...')
 })
 
